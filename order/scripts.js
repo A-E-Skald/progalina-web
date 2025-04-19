d8184de47cf43d1310bd6731bacb33a32e9a6cf0
A-E-Skald <159257789+A-E-Skald@users.noreply.github.com>
2025-04-19 13:13:43 +0300

    initial commit
    second try

---
+++ order/scripts.js
@@ -0,0 +1,184 @@
+// чтобы при нажатии кнопки добавить в выпадающем списке выбиралась опция
+
+function chooseSoup(value) {
+    document.getElementById('soup').value = value;
+    calculateTotal();
+}
+function chooseMeal(value) {
+    document.getElementById('meal').value = value;
+    calculateTotal();
+}
+function chooseDrink(value) {
+    document.getElementById('drink').value = value;
+    calculateTotal();
+}
+function chooseSalad(value) {
+    document.getElementById('salad').value = value;
+    calculateTotal();
+}
+function chooseDessert(value) {
+    document.getElementById('dessert').value = value;
+    calculateTotal();
+}
+
+// вывод (с фильтром или без)
+
+function renderDishes(dishes, containerId, chooseFunctionName, filter) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = "";
+    for (let dish of dishes) {
+        if((dish.kind == filter) || (filter == null)) {
+            const dishHTML = `
+            <div class="dish">
+                <div class="dish-name">${dish.name}</div>
+                <img class = "img-dish" src="${dish.img}" alt="блюдо">
+                <div class="dish-price">${dish.price} петабайт</div>
+                <p class="p-order">${dish.desc}</p>
+                <a href="#" class="add" onclick="${chooseFunctionName}('${dish.keyword}'); return false;">Добавить</a>
+            </div>
+        `   ;
+            container.insertAdjacentHTML('beforeend', dishHTML);
+        }
+    }
+}
+
+//рассчет стоимости
+
+function calculateTotal() {
+    const soupValue = document.getElementById("soup").value;
+    const mealValue = document.getElementById("meal").value;
+    const drinkValue = document.getElementById("drink").value;
+    const saladValue = document.getElementById("salad").value;
+    const dessertValue = document.getElementById("dessert").value;
+
+    function findDish(arr, keyword) {
+        return arr.find(dish => dish.keyword === keyword);
+    }
+
+    const soup = findDish(soups, soupValue);
+    const meal = findDish(meals, mealValue);
+    const drink = findDish(drinks, drinkValue);
+    const salad = findDish(salads, saladValue);
+    const dessert = findDish(desserts, dessertValue);
+
+    let total = 0;
+    if (soup) total += soup.price;
+    if (meal) total += meal.price;
+    if (drink) total += drink.price;
+    if (salad) total += salad.price;
+    if (dessert) total += dessert.price;
+
+    document.getElementById("total").innerText = "Общая стоимость: " + total + " петабайт";
+    
+}
+
+// начальный рендер без фильтров
+
+renderDishes(soups, "soups", "chooseSoup", null);
+renderDishes(meals, "meals", "chooseMeal", null); 
+renderDishes(drinks, "drinks", "chooseDrink", null);
+renderDishes(salads, "salads", "chooseSalad", null);
+renderDishes(desserts, "desserts", "chooseDessert", null); 
+
+// чтобы при нажатии кнопки добавить в выпадающем списке выбиралась опция
+
+document.getElementById("soup").addEventListener("change", calculateTotal);
+document.getElementById("meal").addEventListener("change", calculateTotal);
+document.getElementById("drink").addEventListener("change", calculateTotal);
+document.getElementById("salad").addEventListener("change", calculateTotal);
+document.getElementById("dessert").addEventListener("change", calculateTotal);
+
+// для работы кнопок с фильтром
+
+const buttons = document.getElementsByClassName("filter");
+const dict_func = {
+    soups: "chooseSoup",
+    meals: "chooseMeal",
+    drinks: "chooseDrink",
+    salads: "chooseSalad",
+    desserts: "chooseDessert",
+}
+const data = {
+    soups: soups,
+    meals: meals,
+    drinks: drinks,
+    salads: salads,
+    desserts: desserts,
+}
+for (let button of buttons) {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+        const button_category = button.parentElement.dataset.category;
+        const arr_name = data[button_category];
+        const kind = button.dataset.kind;
+        const func_name = dict_func[button_category];
+        
+        if (button.classList.contains("active")) {
+            button.classList.remove("active");
+            renderDishes(arr_name, button_category, func_name, null);
+        }
+        else {
+            const parent = button.closest(".filters");
+            const allButtons = parent.querySelectorAll(".filter");
+            allButtons.forEach(b => b.classList.remove("active"));
+
+            button.classList.add("active");
+            renderDishes(arr_name, button_category, func_name, kind);
+        }
+
+    });
+    
+}
+
+// для работы окна с уведомлением 
+
+const openBtn = document.getElementById('openModal');
+const closeBtn = document.getElementById('closeModal');
+const overlay = document.getElementById('modalOverlay');
+
+openBtn.addEventListener('click', () => {
+
+    const soupBin = (!document.getElementById("soup").value == "");
+    const mealBin = (!document.getElementById("meal").value == "");
+    const drinkBin = (!document.getElementById("drink").value == "");
+    const saladBin = (!document.getElementById("salad").value == "");
+    const dessertBin = (!document.getElementById("dessert").value == "");
+
+    console.log(soupBin,mealBin,drinkBin,saladBin,dessertBin);
+    console.log((soupBin && !mealBin) , (soupBin && !saladBin));
+
+
+    if (!soupBin && !mealBin && !saladBin && !drinkBin && !dessertBin){
+        document.getElementById('replacable-text').innerText = 'Ничего не выбрано. Выбирите блюда для заказа.';
+        overlay.style.display = 'flex';
+    }
+
+    else if ((soupBin && mealBin && saladBin && !drinkBin) || (soupBin && saladBin && !drinkBin) || (mealBin && !drinkBin) || (soupBin && mealBin && !drinkBin) || (mealBin && saladBin && !drinkBin)) {
+        document.getElementById('replacable-text').innerText = 'Выберите напиток.';
+        overlay.style.display = 'flex';
+    }
+
+    else if ((soupBin && !mealBin) && (soupBin && !saladBin)) {
+        document.getElementById('replacable-text').innerText = 'Выберите главное блюдо / салат.';   
+        overlay.style.display = 'flex';
+    }
+
+    else if((saladBin && !mealBin) && (saladBin && !soupBin)) {
+        document.getElementById('replacable-text').innerText = 'Выберите главное блюдо / суп.';   
+        overlay.style.display = 'flex';
+    }
+
+    else if((!soupBin && !mealBin && !saladBin && drinkBin) || (!soupBin && !mealBin && !saladBin && !drinkBin && dessertBin)) {
+        document.getElementById('replacable-text').innerText = 'Выберите главное блюдо.';   
+        overlay.style.display = 'flex';
+    }
+
+    else {
+        document.getElementByClassName('form-beautiful')[0].submit();   
+    }
+});
+
+closeBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
